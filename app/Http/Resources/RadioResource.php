eb9c384314bdd76a9e685a4b21033c6b9db5d378
Isaacn123 <nsambai72@gmail.com>
2023-07-04 14:00:23 +0300

    adding category name

--- app/Http/Resources/RadioResource.php
+++ app/Http/Resources/RadioResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class RadioResource extends JsonResource
@@ -17,6 +18,7 @@ class RadioResource extends JsonResource
         return [
             'radio_id' => $this->id,
             'category_id' => $this-> category_id,
+            'category_name' => Category::where('id',$this-> category_id)->first()->category_name,
             'radio_name' => $this->radio_name,
             'radio_image' => $this->radio_image,
             'radio_url' => $this->radio_url,
